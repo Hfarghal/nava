2b85f791dc197b575e66052d10e8a6fbe07d4a12
hussienfarghal <hussienfarghal@gmail.com>
2017-09-03 10:09:52 +0200

    js files

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -12624,4 +12624,13 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
+/*
+ Created By Hussien Mohamed Farghal
+ Copyright (C) 2017 - All Rights Reserved.
+ * You can not use or distribute or modify this code with out my permissions. 
+ Please contact me first throw this channels 
+ E-mail:hussienfarghal@gmail.com
+ Facebook.com/hussienfarghal
+ mobile:+201207077747
+*/
 //custom scripts
